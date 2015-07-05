6e69df2adbb756432ad4d55e99014f959b20395e
endrius <joskowski.andrzej@gmail.com>
2015-07-05 22:06:11 +0200

    PG-32: Wygenerowany szablon dla angularjs+nodejs

---
+++ client/app/main/main.js
@@ -0,0 +1,10 @@
+'use strict';
+
+angular.module('sharpWarsWebServiceApp')
+  .config(function ($routeProvider) {
+    $routeProvider
+      .when('/', {
+        templateUrl: 'app/main/main.html',
+        controller: 'MainCtrl'
+      });
+  });
