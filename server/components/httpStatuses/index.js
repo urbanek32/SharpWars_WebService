16071fdc9425f3d2a2bc43cda95a88a7722fbe7e
endrius <joskowski.andrzej@gmail.com>
2015-07-09 19:18:02 +0200

    PG-44: Dodano generyczne kody bledow
    Signed-off-by: endrius <joskowski.andrzej@gmail.com>

--- server/components/httpStatuses/index.js
+++ server/components/httpStatuses/index.js
@@ -4,6 +4,22 @@
 
 'use strict';
 
+var generic = {
+  InternalServerError: {status: 500, message: "Internal Server Error"}
+};
+
+var users = {
+  //success
+  Created: {status: 201, message: "User created"},
+  //errors
+  NotCreated: {status: 500, message: "Cannot create user"}
+};
+
+module.exports.httpStatuses = {
+  Generic: generic,
+  Users: users
+};
+
 module.exports[404] = function pageNotFound(req, res) {
   var viewFilePath = '404';
   var statusCode = 404;
