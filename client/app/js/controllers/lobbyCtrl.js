8af9e247478a407bab5187553654dc76076ff216
Michał Żyłowski <kermbot@gmail.com>
2015-08-20 01:27:38 +0200

    PG-129: Ekran tworzenia nowego lobby

--- client/app/js/controllers/lobbyCtrl.js
+++ client/app/js/controllers/lobbyCtrl.js
@@ -1,9 +1,22 @@
 'use strict';
 
 angular.module('sharpWarsWebServiceApp')
-  .controller('lobbyCtrl', function ($scope,$window) {
+  .controller('lobbyCtrl', function ($scope, $window, lobbyService, errorInterpreter) {
 
-    $scope.lobbyCreateRequest = function(newLobby) {
-      $window.alert(newLobby);
+    $scope.createLobby = function() {
+      $scope.errors = null;
+      $scope.serverResponse = null;
+      if (!$scope.lobby.encrypted) {
+        $scope.lobby.encrypted = false;
+      }
+      lobbyService.creteLobby($scope.user.name, $scope.lobby, function(err, result) {
+        if(!err && result) {
+          $scope.serverResponse = result[0].message;
+        } else {
+          $scope.errors = errorInterpreter.interpreter(err);
+        }
+      });
     };
   });
+
+
