b4bd0a35478abdf95bb08438114aaabd06eaa889
endrius <joskowski.andrzej@gmail.com>
2015-09-24 18:17:24 +0200

    Dodane testowe ścieżki api

--- server/routes.js
+++ server/routes.js
@@ -12,12 +12,14 @@ module.exports = function(app) {
 
   // Insert routes below
   app.use('/api/users', require('./handlers/users'));
-  app.use('/api/tests', require('./handlers/test_path'));
   app.use('/auth/api/users', expressJwt({secret: config.auth.key}));
   app.use('/auth/api/users', require('./handlers/auth/users'));
   app.use('/auth/api/users/:username/lobby', require('./handlers/auth/lobby'));
   app.use('/auth/api/users/:username/game', require('./handlers/auth/game'));
   app.use('/auth/api/users/:username/scripts', require('./handlers/auth/scripts'));
+
+  //test routes
+  app.use('/test/api/', require('./handlers/test'));
 
   // All undefined asset or api routes should return a 404
   app.route('/:url(api|auth|components|app|bower_components|assets)/*')
